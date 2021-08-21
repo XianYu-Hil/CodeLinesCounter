343a1467afde6a6206cc4cab9baa82de3f4f3a2b
XianYu-Hil <3033619778@qq.com>
2021-08-21 22:57:26 +0800

    using dictionary + debug mode

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ suffixes_libraries = {
     "Visual Basic": ["vb"],
 }
 
+is_debug = False
+
 
 def code_counter(path, suffixes):
     code_line_number = 0
@@ -18,7 +20,7 @@ def code_counter(path, suffixes):
         for file in files:
             for suffix in suffixes:
                 if file.endswith(suffix):
-                    # print(os.path.join(root, file))
+                    if is_debug: print(os.path.join(root, file))
                     code_file_number += 1
                     code_file = open(os.path.join(root, file), "r", encoding='utf-8')
                     for line in code_file.readlines():
@@ -29,31 +31,21 @@ def code_counter(path, suffixes):
 while True:
     print("=========================")
     print()
-    print("请输入你的语言类型")
-    language_type = input()
+    language_type = input("请输入你的语言类型")
     if language_type == "exit":
         break
-    print("请输入你的代码路径")
-    code_path = input()
+    elif language_type == "debug":
+        print("Debug mode " + str(not is_debug) + ".")
+        is_debug = not is_debug
+        continue
+    code_path = input("请输入你的代码路径")
     print()
     print("计算中......")
     print()
     start_time = time.time()
 
-    if language_type == "C#":
-        result = code_counter(code_path, ["cs"])
-        print("该项目共" + str(result[0]) + "个文件," + str(result[1]) + "行代码")
-    elif language_type == "Python":
-        result = code_counter(code_path, ["py", "pyw"])
-        print("该项目共" + str(result[0]) + "个文件," + str(result[1]) + "行代码")
-    elif language_type == "C":
-        result = code_counter(code_path, ["h", "c"])
-        print("该项目共" + str(result[0]) + "个文件," + str(result[1]) + "行代码")
-    elif language_type == "C++":
-        result = code_counter(code_path, ["h", "hpp", "hxx", "cpp", "cxx", "cc", "c", "c++"])
-        print("该项目共" + str(result[0]) + "个文件," + str(result[1]) + "行代码")
-    elif language_type == "VisualBasic":
-        result = code_counter(code_path, ["vb"])
+    if language_type.lower() in suffixes_libraries:
+        result = code_counter(code_path, suffixes_libraries[language_type])
         print("该项目共" + str(result[0]) + "个文件," + str(result[1]) + "行代码")
     else:
         print("未知语言:" + language_type)
